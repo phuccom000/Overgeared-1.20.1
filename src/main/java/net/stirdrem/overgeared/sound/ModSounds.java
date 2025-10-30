6633b4bedae1ee5ba57e428deefc7317e6c0c156
BF C <hd36099@gmail.com>
2025-10-30 18:52:38 +0700

    ModSounds

--- src/main/java/net/stirdrem/overgeared/sound/ModSounds.java
+++ src/main/java/net/stirdrem/overgeared/sound/ModSounds.java
@@ -1,29 +1,36 @@
 package net.stirdrem.overgeared.sound;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraftforge.common.util.ForgeSoundType;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+
+//import net.minecraftforge.common.util.ForgeSoundType;
+//import net.minecraftforge.eventbus.api.IEventBus;
+//import net.minecraftforge.registries.DeferredRegister;
+//import net.minecraftforge.registries.ForgeRegistries;
+//import net.minecraftforge.registries.RegistryObject;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import net.stirdrem.overgeared.OvergearedMod;
+
+import java.util.function.Supplier;
 
 public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
-            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, OvergearedMod.MOD_ID);
+            DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, OvergearedMod.MOD_ID);
 
-    /*public static final RegistryObject<SoundEvent> METAL_DETECTOR_FOUND_ORE = registerSoundEvents("metal_detector_found_ore");
+    /*public static final Supplier<SoundEvent> METAL_DETECTOR_FOUND_ORE = registerSoundEvent("metal_detector_found_ore");
 
-    public static final RegistryObject<SoundEvent> SOUND_BLOCK_BREAK = registerSoundEvents("sound_block_break");
-    public static final RegistryObject<SoundEvent> SOUND_BLOCK_STEP = registerSoundEvents("sound_block_step");
-    public static final RegistryObject<SoundEvent> SOUND_BLOCK_FALL = registerSoundEvents("sound_block_fall");
-    public static final RegistryObject<SoundEvent> SOUND_BLOCK_PLACE = registerSoundEvents("sound_block_place");
-    public static final RegistryObject<SoundEvent> SOUND_BLOCK_HIT = registerSoundEvents("sound_block_hit");*/
+    public static final Supplier<SoundEvent> SOUND_BLOCK_BREAK = registerSoundEvent("sound_block_break");
+    public static final Supplier<SoundEvent> SOUND_BLOCK_STEP = registerSoundEvent("sound_block_step");
+    public static final Supplier<SoundEvent> SOUND_BLOCK_FALL = registerSoundEvent("sound_block_fall");
+    public static final Supplier<SoundEvent> SOUND_BLOCK_PLACE = registerSoundEvent("sound_block_place");
+    public static final Supplier<SoundEvent> SOUND_BLOCK_HIT = registerSoundEvent("sound_block_hit");*/
 
-    public static final RegistryObject<SoundEvent> ANVIL_HIT = registerSoundEvents("anvil_hit");
-    public static final RegistryObject<SoundEvent> FORGING_COMPLETE = registerSoundEvents("forging_complete");
-    public static final RegistryObject<SoundEvent> FORGING_FAILED = registerSoundEvents("forging_failed");
+    public static final Supplier<SoundEvent> ANVIL_HIT = registerSoundEvent("anvil_hit");
+    public static final Supplier<SoundEvent> FORGING_COMPLETE = registerSoundEvent("forging_complete");
+    public static final Supplier<SoundEvent> FORGING_FAILED = registerSoundEvent("forging_failed");
 
 
     /*public static final ForgeSoundType SOUND_BLOCK_SOUNDS = new ForgeSoundType(1f, 1f,
@@ -31,8 +38,9 @@ public class ModSounds {
             ModSounds.SOUND_BLOCK_HIT, ModSounds.SOUND_BLOCK_FALL);*/
 
 
-    private static RegistryObject<SoundEvent> registerSoundEvents(String name) {
-        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(ResourceLocation.tryBuild(OvergearedMod.MOD_ID, name)));
+    private static Supplier<SoundEvent> registerSoundEvent(String name) {
+        ResourceLocation id = ResourceLocation.fromNamespaceAndPath(OvergearedMod.MOD_ID, name);
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(id));
     }
 
     public static void register(IEventBus eventBus) {
