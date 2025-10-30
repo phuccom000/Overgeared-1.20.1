f38bf88c41a85a6701acfcd8392d942d0cbc7e34
BF C <hd36099@gmail.com>
2025-10-30 18:52:52 +0700

    ModToolTiers

--- src/main/java/net/stirdrem/overgeared/item/ModToolTiers.java
+++ src/main/java/net/stirdrem/overgeared/item/ModToolTiers.java
@@ -6,22 +6,29 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraftforge.common.ForgeTier;
-import net.minecraftforge.common.Tags;
-import net.minecraftforge.common.TierSortingRegistry;
+//import net.minecraftforge.common.ForgeTier;
+//import net.minecraftforge.common.Tags;
+//import net.minecraftforge.common.TierSortingRegistry;
+import net.neoforged.neoforge.common.SimpleTier;
 import net.stirdrem.overgeared.OvergearedMod;
 import net.stirdrem.overgeared.util.ModTags;
 
 import java.util.List;
 
 public class ModToolTiers {
-    public static final Tier STEEL = TierSortingRegistry.registerTier(
-            new ForgeTier(2, 500, 7.0F, 3.0F, 12,
-                    ModTags.Blocks.NEEDS_STEEL_TOOL, () -> Ingredient.of(ModItems.STEEL_INGOT.get())),
-            ResourceLocation.tryBuild(OvergearedMod.MOD_ID, "steel_ingot"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
 
-    public static final Tier COPPER = TierSortingRegistry.registerTier(
-            new ForgeTier(1, 190, 5.0F, 1.0F, 12,
-                    ModTags.Blocks.NEEDS_COPPER_TOOL, () -> Ingredient.of(Items.COPPER_INGOT)),
-            ResourceLocation.tryBuild(OvergearedMod.MOD_ID, "copper_ingot"), List.of(Tiers.STONE), List.of(Tiers.IRON));
+    public static final Tier STEEL = new SimpleTier(
+            ModTags.Blocks.NEEDS_STEEL_TOOL,
+            500,
+            7f,
+            3f,
+            12,
+            () -> Ingredient.of(ModItems.STEEL_INGOT));
+    public static final Tier COPPER = new SimpleTier(
+            ModTags.Blocks.NEEDS_STEEL_TOOL,
+            500,
+            7f,
+            3f,
+            12,
+            () -> Ingredient.of(ModItems.STEEL_INGOT));
 }
