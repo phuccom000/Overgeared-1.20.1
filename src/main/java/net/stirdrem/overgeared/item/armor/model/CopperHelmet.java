bd3e8405d7f61d10d0dca37f33cd8bec114c896a
Arimil <renari@arimil.com>
2025-12-31 20:03:44 -0500

    formatting

--- src/main/java/net/stirdrem/overgeared/item/armor/model/CopperHelmet.java
+++ src/main/java/net/stirdrem/overgeared/item/armor/model/CopperHelmet.java
@@ -16,48 +16,48 @@ import net.minecraft.world.entity.Entity;
 import net.stirdrem.overgeared.OvergearedMod;
 
 public class CopperHelmet<T extends Entity> extends EntityModel<T> {
-    // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(OvergearedMod.MOD_ID, "copper_helmet"), "main");
-    public final ModelPart Head;
+  // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+  public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(OvergearedMod.MOD_ID, "copper_helmet"), "main");
+  public final ModelPart Head;
 
-    public CopperHelmet(ModelPart root) {
-        this.Head = root.getChild("Head");
-    }
+  public CopperHelmet(ModelPart root) {
+    this.Head = root.getChild("Head");
+  }
 
-    public static LayerDefinition createBodyLayer() {
-        MeshDefinition meshdefinition = new MeshDefinition();
-        PartDefinition partdefinition = meshdefinition.getRoot();
+  public static LayerDefinition createBodyLayer() {
+    MeshDefinition meshdefinition = new MeshDefinition();
+    PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(1.0F))
-                .texOffs(34, -15).addBox(0.0F, -15.0F, -5.0F, 0.0F, 6.0F, 15.0F, new CubeDeformation(0.0F))
-                .texOffs(54, 1).addBox(0.0F, -9.0F, 5.0F, 0.0F, 2.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+    PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(1.0F))
+            .texOffs(34, -15).addBox(0.0F, -15.0F, -5.0F, 0.0F, 6.0F, 15.0F, new CubeDeformation(0.0F))
+            .texOffs(54, 1).addBox(0.0F, -9.0F, 5.0F, 0.0F, 2.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        PartDefinition cube_r1 = Head.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, -0.1963F, 0.0F));
+    PartDefinition cube_r1 = Head.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, -0.1963F, 0.0F));
 
-        PartDefinition cube_r2 = Head.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, -0.3927F, 0.0F));
+    PartDefinition cube_r2 = Head.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, -0.3927F, 0.0F));
 
-        PartDefinition cube_r3 = Head.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, 0.1963F, 0.0F));
+    PartDefinition cube_r3 = Head.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, 0.1963F, 0.0F));
 
-        PartDefinition cube_r4 = Head.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, 0.3927F, 0.0F));
+    PartDefinition cube_r4 = Head.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(54, 3).addBox(0.0F, -7.0F, -1.0F, 0.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -8.0F, 6.0F, 0.0F, 0.3927F, 0.0F));
 
-        PartDefinition cube_r5 = Head.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, -0.1963F));
+    PartDefinition cube_r5 = Head.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, -0.1963F));
 
-        PartDefinition cube_r6 = Head.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, -0.3927F));
+    PartDefinition cube_r6 = Head.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, -0.3927F));
 
-        PartDefinition cube_r7 = Head.addOrReplaceChild("cube_r7", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, 0.1963F));
+    PartDefinition cube_r7 = Head.addOrReplaceChild("cube_r7", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, 0.1963F));
 
-        PartDefinition cube_r8 = Head.addOrReplaceChild("cube_r8", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, 0.3927F));
+    PartDefinition cube_r8 = Head.addOrReplaceChild("cube_r8", CubeListBuilder.create().texOffs(34, -15).addBox(0.0F, -5.0F, -5.0F, 0.0F, 5.0F, 15.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, 0.0F, 0.0F, 0.0F, 0.3927F));
 
-        return LayerDefinition.create(meshdefinition, 64, 32);
-    }
+    return LayerDefinition.create(meshdefinition, 64, 32);
+  }
 
-    @Override
-    public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+  @Override
+  public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 
-    }
+  }
 
-    @Override
-    public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
-        Head.render(poseStack, buffer, packedLight, packedOverlay, color);
-    }
+  @Override
+  public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
+    Head.render(poseStack, buffer, packedLight, packedOverlay, color);
+  }
 }
--- src/main/java/net/stirdrem/overgeared/item/armor/model/CopperLeggings.java
+++ src/main/java/net/stirdrem/overgeared/item/armor/model/CopperLeggings.java
@@ -1,4 +1,8 @@
-package net.stirdrem.overgeared.item.armor.model;// Made with Blockbench 4.12.5
+package net.stirdrem.overgeared.item.armor.model;
+// Made with Blockbench 5.0.5
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
@@ -10,49 +14,45 @@ import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.stirdrem.overgeared.OvergearedMod;
-// Made with Blockbench 4.12.5
-// Exported for Minecraft version 1.17 or later with Mojang mappings
-// Paste this class into your mod and generate all required imports
-
 
 public class CopperLeggings<T extends Entity> extends EntityModel<T> {
-    // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(OvergearedMod.MOD_ID, "copper_leggings"), "main");
-    public final ModelPart Body;
-    public final ModelPart RightLeg;
-    public final ModelPart LeftLeg;
+  // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+  public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(OvergearedMod.MOD_ID, "copper_leggings"), "main");
+  public final ModelPart Body;
+  public final ModelPart RightLeg;
+  public final ModelPart LeftLeg;
 
-    public CopperLeggings(ModelPart root) {
-        this.Body = root.getChild("Body");
-        this.RightLeg = root.getChild("RightLeg");
-        this.LeftLeg = root.getChild("LeftLeg");
-    }
+  public CopperLeggings(ModelPart root) {
+    this.Body = root.getChild("Body");
+    this.RightLeg = root.getChild("RightLeg");
+    this.LeftLeg = root.getChild("LeftLeg");
+  }
 
-    public static LayerDefinition createBodyLayer() {
-        MeshDefinition meshdefinition = new MeshDefinition();
-        PartDefinition partdefinition = meshdefinition.getRoot();
+  public static LayerDefinition createBodyLayer() {
+    MeshDefinition meshdefinition = new MeshDefinition();
+    PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition Body = partdefinition.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.51F))
-                .texOffs(16, 0).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.65F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+    PartDefinition Body = partdefinition.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.51F))
+            .texOffs(16, 0).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.645F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        PartDefinition RightLeg = partdefinition.addOrReplaceChild("RightLeg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.5F))
-                .texOffs(40, 0).addBox(-2.1F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.65F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
+    PartDefinition RightLeg = partdefinition.addOrReplaceChild("RightLeg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.5F))
+            .texOffs(40, 0).addBox(-2.1F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.65F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
 
-        PartDefinition LeftLeg = partdefinition.addOrReplaceChild("LeftLeg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.5F)).mirror(false)
-                .texOffs(40, 0).addBox(-5.9F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.65F)), PartPose.offset(1.9F, 12.0F, 0.0F));
+    PartDefinition LeftLeg = partdefinition.addOrReplaceChild("LeftLeg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.5F)).mirror(false)
+            .texOffs(40, 0).addBox(-5.9F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.65F)), PartPose.offset(1.9F, 12.0F, 0.0F));
 
-        return LayerDefinition.create(meshdefinition, 64, 32);
-    }
+    return LayerDefinition.create(meshdefinition, 64, 32);
+  }
 
-    @Override
-    public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+  @Override
+  public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 
-    }
+  }
 
-    @Override
-    public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, int color) {
-        Body.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
-        RightLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
-        LeftLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
-    }
+  @Override
+  public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
+    Body.render(poseStack, buffer, packedLight, packedOverlay, color);
+    RightLeg.render(poseStack, buffer, packedLight, packedOverlay, color);
+    LeftLeg.render(poseStack, buffer, packedLight, packedOverlay, color);
+  }
 }
